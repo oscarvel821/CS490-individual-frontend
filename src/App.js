50dd0bf8a1b0f4fe0a29313ca5e49d5792f51747
Oscar Velazquez <oscarvel821@gmail.com>
2023-09-12 22:41:28 -0400

    added proxy and created rest call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
+  return(
+    <div>
+      {(typeof backendData.title === 'undefined') ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>{backendData.title}</h1>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
